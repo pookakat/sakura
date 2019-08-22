e93cb427fbdc6b884afb9fe190ec4ae0e2bd3b73
Katie Humburg <pookakat@gmail.com>
2019-08-22 17:31:25 -0400

    resolve EACCES error

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 });
 
-app.listen(80, function () {
-  console.log(`CORS-enabled web server listening on port 80`)
+app.listen(3800, function () {
+  console.log(`CORS-enabled web server listening on port 3800`)
 });
 
 //app.use(express.urlencoded({ extended: true }));
