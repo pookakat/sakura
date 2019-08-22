496560c76e88e696c9b19af21fcbde57f90ff57d
Katie Humburg <pookakat@gmail.com>
2019-08-22 17:39:54 -0400

    change listen port back for CORS

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 });
 
-app.listen(3800, function () {
-  console.log(`CORS-enabled web server listening on port 3800`)
+app.listen(80, function () {
+  console.log(`CORS-enabled web server listening on port 80`)
 });
 
 //app.use(express.urlencoded({ extended: true }));
