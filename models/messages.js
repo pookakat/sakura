7e98badcc685f966bdc9318e8d8f45144ab0d855
Katie Humburg <pookakat@gmail.com>
2019-08-22 17:07:36 -0400

    modify the other model, too

--- models/messages.js
+++ models/messages.js
@@ -24,22 +24,5 @@ module.exports = function(sequelize, DataTypes){
             freezeTableName: true
         });
 
-    Messages.sync().then(()=>{
-        Messages.create({
-            userInit: 1,
-            userReceiving: 2,
-            message: "hi bob how are you?"
-        })
-        Messages.create({
-            userInit: 2,
-            userReceiving: 1,
-            message: "pretty good, how do I eat a potato?"
-        })
-        Messages.create({
-            userInit: 1,
-            userReceiving: 3,
-            message: "Bob is a bit weird."
-        })
-    });
-
+   return Messages;
 };
