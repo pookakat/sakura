31455d2b704014937274c62b63f2edaa31dee48b
Katie Humburg <pookakat@gmail.com>
2019-08-22 17:12:27 -0400

    put the user profiles seeds where the messages ones are

--- models/messages.js
+++ models/messages.js
@@ -24,5 +24,22 @@ module.exports = function(sequelize, DataTypes){
             freezeTableName: true
         });
 
-   return Messages;
+    Messages.sync().then(()=>{
+        Messages.create({
+            userInit: 1,
+            userReceiving: 2,
+            message: "hi bob how are you?"
+        })
+        Messages.create({
+            userInit: 2,
+            userReceiving: 1,
+            message: "pretty good, how do I eat a potato?"
+        })
+        Messages.create({
+            userInit: 1,
+            userReceiving: 3,
+            message: "Bob is a bit weird."
+        })
+    });
+
 };
