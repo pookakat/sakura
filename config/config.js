22815afa19412172dddf0a5bab1ea65c31528bca
Katie Humburg <pookakat@gmail.com>
2019-08-22 15:14:54 -0400

    try to resolve dialect error

--- config/config.js
+++ config/config.js
@@ -12,7 +12,6 @@ module.exports = {
       "database": "database_test",
       "host": "127.0.0.1",
       "dialect": "mysql",
-      "operatorsAliases": false
     },
     "production": {
       "use_env_variable": "JAWSDB_URL",
