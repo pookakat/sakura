5531f2469b80bcbd48f2aca27124a862f70a438c
Katie Humburg <pookakat@gmail.com>
2019-08-22 16:44:48 -0400

    try another thing

--- models/index.js
+++ models/index.js
@@ -15,11 +15,10 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     var model = sequelize['import'](path.join(__dirname, file));
@@ -31,6 +30,7 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
