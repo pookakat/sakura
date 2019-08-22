94fb463d056487bd389e7636499a5a8e82c878e2
Katie Humburg <pookakat@gmail.com>
2019-08-22 19:22:09 -0400

    define db

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "production";
 var config = require(__dirname + "/../config/config.js")[env];
 let models = {};
+var db = {};
 
 
 if (config.use_env_variable) {
