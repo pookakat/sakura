f1ba9121670ae6274092de2305935db373216bfb
Katie Humburg <pookakat@gmail.com>
2019-08-22 00:46:24 -0400

    try to follow directions again

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
-
+var mysql = require('mysql');
 var Sequelize = require("sequelize");
+var connection;
 const jawsdb = process.env.JAWSDB_URL;
 if (jawsdb) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
   var sequelize = new Sequelize(jawsdb);
   console.log('running jawsDB', jawsdb);
 } else {
